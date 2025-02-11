6e14408ea09a60b2aae2b936ded5c8b7850c36f6
hackma01 <amanmourya295@gmail.com>
2025-02-11 11:44:00 +0530

    updated user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -53,9 +53,14 @@ const getUser = async (req,res) => {
     try{
        const { email } = req.body;
        const user = await User.findOne({email});
+
+       if(!user){
+        return res.status(400).json({message: "user not found"})
+       }
+
        res.status(200).json(user); 
     } catch(error){
-        res.status(500).json({message : "Can't get the user!"});
+        res.status(500).json({message : "Can't get the user!",error: error.message});
     }
 }
 
